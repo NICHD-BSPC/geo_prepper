339f69dc59402bb4f102da132d36ac631a22ca8a
Apratim Mitra <apratim.mitra@nih.gov>
2020-12-16 10:16:39 -0500

    adding general path to requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
+import os
 import setuptools
 
 long_description = """
 Prepare data for NCBI GEO submission
 """
+requirements = open(os.path.join(os.path.dirname(__file__), 'requirements.txt')).readlines()
 
 setuptools.setup(
     name="geo-prepper",
@@ -13,7 +15,7 @@ setuptools.setup(
     url="http://github.com/NICHD-BSPC/geo-prepper",
     packages=['geo-prepper'],
     package_dir={'geo-prepper': 'geo-prepper'},
-    install_requires=[i.strip() for i in open('requirements.txt') if not i.startswith('#')],
+    install_requires=requirements,
     classifiers=[
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",

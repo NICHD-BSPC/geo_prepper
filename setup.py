bffa1e01557b750f9e4b5e1ba2040ab014187063
Apratim Mitra <apratim.mitra@nih.gov>
2020-12-16 15:29:19 -0500

    fix content type; change repo name to geo_prepper

--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
 import os
 import setuptools
 
-with open("README.rst", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 requirements = open(os.path.join(os.path.dirname(__file__), 'requirements.txt')).readlines()
 
 setuptools.setup(
-    name="geo-prepper",
+    name="geo_prepper",
     version="0.0.1",
     author="Apratim Mitra",
     author_email="apratim.mitra@nih.gov",
-    description=long_description,
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
+    description="GEO submission prepper",
+    description_content_type="text/plain",
+    long_description="Tool to prepare data for NCBI GEO submission",
+    long_description_content_type="text/plain",
     license='GNU GPLv3',
-    url="http://github.com/NICHD-BSPC/geo-prepper",
-    packages=['geo-prepper'],
-    package_dir={'geo-prepper': 'geo-prepper'},
+    url="http://github.com/NICHD-BSPC/geo_prepper",
+    packages=['geo_prepper'],
+    package_dir={'geo_prepper': 'geo_prepper'},
     install_requires=requirements,
     classifiers=[
         "License :: OSI Approved :: GNU General Public License (GPL)",
